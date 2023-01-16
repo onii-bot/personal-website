79a218e5711a7d83df930ff9c3e7548646641357
onii <fabianmann9@gmail.com>
2023-01-16 17:58:52 +0545

    added the links to each element in contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,11 +3,11 @@
         <div id="firstbox" class="w-[50%]">
             <div v-for="item in contacts.slice(0, 3)" :key="item.id" v-on:mouseover="item.isHovered = true"
                 v-on:mouseleave="item.isHovered = false" class="h-[33%] px-2 py-4 mx-2 my-4 font-mono text-lg">
-                <span class="">{{ item.id }}</span>
-                <span :class="{ 'text-palette': !item.isHovered }" v-on:mouseover="item.isHovered = true"
+                    <span class="">{{ item.id }}</span>
+                    <a :href="item.link" target="_blank" rel="noopener noreferrer"><span :class="{ 'text-palette': !item.isHovered }" v-on:mouseover="item.isHovered = true"
                     v-on:mouseleave="item.isHovered = false">
                     {{ item.title }}
-                </span>
+                    </span></a>
             </div>
         </div>
         <div class="">
@@ -16,10 +16,10 @@
         <div id="secondbox" class="w-[50%] text-right">
             <div v-for="item in contacts.slice(3, 6)" :key="item.id" v-on:mouseover="item.isHovered = true"
                 v-on:mouseleave="item.isHovered = false" class="h-[33%] px-2 py-4 mx-2 my-4 font-mono text-lg">
-                <span :class="{ 'text-palette': !item.isHovered }">
+                <a :href="item.link" target="_blank" rel="noopener noreferrer"><span :class="{ 'text-palette': !item.isHovered }">
                     {{ item.title }}
-                </span>
-                <span class="">{{ item.id }}</span>
+                    </span></a>
+                    <span class="">{{ item.id }}</span>
             </div>
         </div>
     </div>
@@ -31,12 +31,12 @@ export default {
     data() {
         return {
             contacts: [
-                { "id": "1.", "title": " Github", "isHovered": false },
-                { "id": "2.", "title": " Discord", "isHovered": false },
-                { "id": "3.", "title": " Twitter", "isHovered": false },
-                { "id": " 4.", "title": "Instagram", "isHovered": false },
-                { "id": " 5.", "title": "Gmail", "isHovered": false },
-                { "id": " 6.", "title": "Linkedin", "isHovered": false }
+                { "id": "1.", "title": " Github", "isHovered": false , "link":"https://github.com/onii-bot"},
+                { "id": "2.", "title": " Discord", "isHovered": false , "link":"https://discordapp.com/users/776716251997274112"},
+                { "id": "3.", "title": " Twitter", "isHovered": false , "link":"https://twitter.com"},
+                { "id": " 4.", "title": "Instagram", "isHovered": false , "link":"https://www.instagram.com/ash._ua/"},
+                { "id": " 5.", "title": "Gmail", "isHovered": false , "link":"mailto: aayushpaudel311@gmail.com"},
+                { "id": " 6.", "title": "Linkedin", "isHovered": false , "link":"https://linkedin.com"}
             ],
 
         }
