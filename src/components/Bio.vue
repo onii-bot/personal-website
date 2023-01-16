81fe497970afb37924d1cde020aead2879c5a51a
onii <fabianmann9@gmail.com>
2023-01-16 20:48:04 +0545

    more responsiveness for phone

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="box" class="mx-16 px-8 py-6 h-[32rem] flex flex-wrap border-neutral-800 border-2">
+    <div id="box" class="mx-16 px-8 py-6 flex flex-row flex-wrap border-neutral-800 border-2">
         <div id="firstbox" class="w-[70%]">
             <h1 class="text-[7rem]">Aayush Paudel</h1>
             <p class="text-[1.15rem] font-mono antialiased">I am a developer with a passion for Python programming. I am currently expanding my skills by learning web design and always looking for new challenges to improve my abilities in this field. You can connect with me on the links below.</p>
@@ -12,8 +12,8 @@
                 <a href="https://www.linkedin.com/in/aayush-paudel-4a1731227/" target="_blank" rel="noopener noreferrer"><img class="cursor-pointer h-8 w-8" :src="require('@/assets/linkedin_logo.png')"></a>
             </div>
         </div>
-        <div id="secondbox" class="w-[30%]">
-            <img :src="require('@/assets/ash.gif')" class="w-[32rem] h-[29rem] mx-2 mb-4">
+        <div id="secondbox" class="">
+            <img :src="require('@/assets/ash.gif')" class="w-[23rem] h-[35rem] mx-2 mb-4">
         </div>
     </div>
 </template>
